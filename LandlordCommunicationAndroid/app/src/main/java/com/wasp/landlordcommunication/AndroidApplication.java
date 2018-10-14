382b22c101f0cb12e476112b3b9fa7abcb8d3219
Nikolay Mateev <nmateev@abv.bg>
2018-10-14 19:23:03 +0300

    Add dagger injection setup

---
+++ LandlordCommunicationAndroid/app/src/main/java/com/wasp/landlordcommunication/AndroidApplication.java
@@ -0,0 +1,15 @@
+package com.wasp.landlordcommunication;
+
+import com.wasp.landlordcommunication.diconfig.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class AndroidApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+
+        return DaggerAppComponent.builder().application(this).build();
+
+    }
+}
---
+++ LandlordCommunicationAndroid/app/src/main/java/com/wasp/landlordcommunication/diconfig/ApplicationModule.java
@@ -0,0 +1,14 @@
+package com.wasp.landlordcommunication.diconfig;
+
+import android.app.Application;
+import android.content.Context;
+
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class ApplicationModule {
+
+    @Binds
+    abstract Context bindContext(Application application);
+}
